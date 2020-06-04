e9fd84a95403a24f0849b44bee02590c4aedbf28
Vishwas Gajbhiye <vishwas123v@gmail.com>
2020-06-04 19:39:17 +0530

    swagger client 3 changes

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 "use strict";
-var SwaggerClient = require("swagger-client");
-var DeltaAPIKeyAuthorization = require("./lib/DeltaAPIKeyAuthorization");
+const SwaggerClient = require("swagger-client");
+const DeltaAPIKeyAuthorization = require("./lib/DeltaAPIKeyAuthorization");
+// const SWAGGER_URL = "http://localhost:8000/openapi.json";
+const SWAGGER_URL = "http://localhost:8000/openapi.json";
+
+const authorization = new DeltaAPIKeyAuthorization('apiKey', 'apiSecret')
 
 const DeltaRestClient = function(api_key, api_secret) {
-  const SWAGGER_URL = "http://localhost:8000/openapi.json";
-  const temp = new DeltaAPIKeyAuthorization(api_key, api_secret);
-  return new SwaggerClient(SWAGGER_URL, {
-      authorizations: {
-        'api-key': temp
+  const authorization = new DeltaAPIKeyAuthorization(api_key, api_secret)
+  return new SwaggerClient({
+    url: SWAGGER_URL,
+    requestInterceptor(req) {
+      if (!req.method) {
+        req.method = "GET"
       }
-    })
-    .then(function(client) {
 
-      // console.log('1111 222 client: ', client)
-
-      // client.clientAuthorizations.add(
-      //   "api-key",
-      //   new DeltaAPIKeyAuthorization(api_key, api_secret)
-      // );
-      return Promise.resolve(client);
-    })
-    .catch(function(e) {
-      console.error("Unable to load swagger json:", e);
-    });
+      if (typeof authorization !== 'undefined') {
+        authorization.apply(req)
+      }
+    }
+  })
+  .then(client => {
+    return Promise.resolve(client);
+  })
+  .catch(function(e) {
+    console.error("Unable to connect: ", e);
+  })
 };
 
 module.exports = DeltaRestClient;
